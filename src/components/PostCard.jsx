0c9cb4a0de02ed80b2cc47a5009ac430952e15ef
Irfan Ahmad <meirfan26@gmail.com>
2022-12-23 00:16:58 +0530

    facebook clone

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -5,7 +5,7 @@ const PostCard = ({src,title,description}) => {
   return (
     <div className='card'>
         <img src={src} />
-        <div className='card_sinfo'>
+        <div className='card_info'>
             <h2>{title}</h2>
             <p>{description}</p>
         </div>
